25b708ed6876396a1215524f285c500ab50306dc
Bruce Tate <bruce@rapidred.com>
2020-12-10 17:17:17 -0500

    initial genserver

---
+++ lib/clock/server.ex
@@ -0,0 +1,53 @@
+defmodule Clock.Server do
+  use GenServer
+  alias Clock.{LEDAdapter, Core}
+
+  def start_link(initial_state \\ default_options()) do
+    GenServer.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
+  def init(args) do
+    pins = args[:pins]
+    time = args[:time] || default_time()
+    send_ticks_fn = args[:send_ticks_fn] || (&default_send_ticks/0)
+    led_adapter = args[:led_adapter]
+
+    send_ticks_fn.()
+    {:ok, Core.new(open_gpio_pins(led_adapter, pins), time)}
+  end
+
+  def handle_info(:tick, clock) do
+    {
+      :noreply, 
+      clock 
+      |> Core.tick() 
+      |> set_leds()
+    }
+  end
+
+  def default_pins, do: %{0 => 26, 1 => 13, 2 => 6, 3 => 5, 4 => 16, 5 => 12}
+  def default_time, do: Time.utc_now().second
+  def default_send_ticks, do: :timer.send_interval(1000, :tick)
+
+  def default_options do
+    %{pins: default_pins(), time: default_time(), send_ticks_fn: &default_send_ticks/0}
+  end
+
+  defp open_gpio_pins(module, pins) do
+    IO.inspect module
+    IO.inspect pins
+    0..5
+    |> Enum.map(fn bit ->
+      {bit, LEDAdapter.open(pins[bit], module)}
+    end)
+    |> Map.new()
+  end
+
+  defp set_leds(clock) do
+    Enum.each(
+      Core.status(clock), 
+      fn {led, state} -> LEDAdapter.set(led, state) end
+    )
+    clock
+  end
+end
